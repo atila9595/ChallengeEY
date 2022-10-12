848bd19dcf8c00cc6f0c01d77ada4229f54fed91
atila9595 <batatas.churros@gmail.com>
2022-10-12 03:12:21 -0300

    criando lista de missão!

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const user_rotas = express.Router()
 const Usuario = require('../models/usuario-model')
+const Missao = require('../models/missao-model')
 
 
 
-user_rotas.get('/', (req, res) => {
-    res.render('usuario/missoes')
+user_rotas.get('/', async(req, res) => {
+    await Missao.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then((missoes) => {
+        console.log(missoes.missao.dataValues.titulo)
+        res.render('usuario/missoes', { missoes: missoes })
+    })
+
 })
 
 user_rotas.get('/index', (req, res) => {
